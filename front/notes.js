a947fec3dafe3a18f63655557cdbf42c9ee483d1
Hemavarshini-alt <hemavarshi218@gmail.com>
2026-02-23 13:39:18 +0530

    fs
    Signed-off-by: Hemavarshini-alt <hemavarshi218@gmail.com>

--- front/notes.js
+++ front/notes.js
@@ -22,7 +22,7 @@ if(quoteText){
 }
 
 // Backend API URL
-const API_URL = "http://localhost:3000/api/tasks";
+const API_URL = "https://notes-backend-3jz5.onrender.com/api/tasks";
 
 // Tasks CRUD
 const tasksTableBody = document.querySelector("#tasks-table tbody");
